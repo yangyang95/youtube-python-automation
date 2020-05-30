0d622261f90bf5a2824ca5d2cd2acdb98016f6cd
Jerry Weng <yangyang.Weng@gmail.com>
2020-05-30 21:34:47 +0800

    Add client secret template

---
+++ secrets.py
@@ -0,0 +1,6 @@
+# Spotify token expire very fast
+# To update, checkout: https://developer.spotify.com/console/post-playlist-tracks/?playlist_id=&position=&uris=
+# check scopes: (1) playlist-modify-public (2) playlist-read-private (3) playlist-read-private (4) playlist-read-collabrative
+
+spotify_token = ""
+spotify_user_id = ""
